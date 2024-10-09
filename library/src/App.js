62830535467ca23ef3d569ce5d91524db52ee898
ll_git <minareg37@gmail.com>
2024-10-09 20:27:08 +0200

    Made the success page

--- library/src/App.js
+++ library/src/App.js
@@ -1,17 +1,15 @@
-import './App.css';
-import Login from './Login';
-import Success from './Success';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import "./App.css";
+import Login from "./Login";
+import Success from "./Success";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
   return (
-  <Router>
-    <Routes>
-      <Route path='/' element={<Login />}/>
-      <Route path='/success' element={<Success />}/>
-
-    </Routes>
-  </Router>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/success" element={<Success />} />
+      </Routes>
+    </Router>
   );
 }
-
 export default App;
