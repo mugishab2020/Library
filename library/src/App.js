55a7b1e9be72517820692804fe9cc63bfc81fd81
mugishab2020 <mugishab2020@gmail.com>
2024-10-14 17:05:40 +0200

    trying to pull

--- library/src/App.js
+++ library/src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
 import Login from './Login';
-import Success from './Success';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 function App() {
   return (
   <Router>
     <Routes>
       <Route path='/' element={<Login />}/>
-      <Route path='/success' element={<Success />}/>
 
     </Routes>
   </Router>
